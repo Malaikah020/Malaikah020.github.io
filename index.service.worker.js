aa8796bf624db466d62191b97c6476c55e2a3a3a
Malaikah Hafeez <Malaikah Hafeez>
2025-03-31 20:07:36 +0100

    new with resources

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743447385|2235776616';
+const CACHE_VERSION = '1743447986|2836836927';
 /** @type {string} */
 const CACHE_PREFIX = 'SusGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
